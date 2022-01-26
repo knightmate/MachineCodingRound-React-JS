ae7cc830b16fbdcf252fd8f4314fd4fea23ac9ea
Meghraj <meghraj.deshmukh103@gmail.com>
2022-01-26 19:53:07 +0530

    Machine coding challenge completed _cars24_ folderStructure implemented

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Folder from './Cars24_folderStructure/index.jsx';
+
+function App() {
+  return (
+    <div className="App">
+       <Folder></Folder>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Cars24_folderStructure/index.jsx
@@ -0,0 +1,106 @@
+ import React from 'react';
+ import folderSchema from '../folderSchema';
+ 
+function temp(itemArr){
+
+    function recursion(item){
+
+        if(item.isFolder){
+            item.items.forEach((it)=>{
+               const ans= recursion(it);
+              
+            });
+        
+        }
+        if(item.isFolder){
+            itemArr.push({Id:item.id,hiddenChild:false})
+        }
+    }
+    recursion(folderSchema);
+
+    return itemArr;
+   
+}
+
+ const Folder = props => {
+
+    const [state ,setState]=React.useState(folderSchema);
+    const [itemIds,setItemIds]=React.useState(()=>{
+       const res= temp([]);
+       console.log('response',res);
+       return res;
+    });
+      
+    
+    
+ function renderView(item,state){
+    let renderedView;
+     if(item.isFolder){
+        renderedView=item.items.map((itm)=>{
+           let viewData=renderView(itm);
+           if(itm.isFolder){
+              return viewData;
+           }
+          return (<div>{viewData.name}</div>)
+        })
+        
+     }
+     if(item.isFolder===false) return item;
+     console.log("shouldDis",itemIds);
+    return (<div id={item.id} onClick={onClick}><h3  id={item.id} style={{margin:"1px",padding:"1px"}}>{item.name}</h3><div  id={item.id}   style={{marginLeft:"10px",display:shouldDisplay(item.id)}}>{renderedView}</div></div>);
+    
+ }
+ 
+  function shouldDisplay(id){
+      console.log("#121","id in should display",id);
+      if(Array.isArray(itemIds)){
+
+      for(let i=0;i<itemIds.length;i++){
+          if(itemIds[i].Id==id){
+
+              console.log(itemIds[i]);
+              return itemIds[i].hiddenChild?"none":"block";
+          }
+      }
+    }else{
+        return "block";
+    }
+ }
+function onClick(event){
+   const itemId=event.target.id;
+    
+   event.stopPropagation();
+   event.preventDefault();
+   console.log("items id",itemId);
+   setItemIds((preState)=>{
+       console.log("PresStat",preState);
+       
+      const res= preState.map((item)=>{
+         
+           if(item.Id==itemId){
+            console.log("#131","itemsID",item);
+            return {
+                Id:item.Id,hiddenChild:item.hiddenChild?false:true
+            }
+             
+           }
+            
+       return {
+           ...item
+        };
+       });
+       return [... res ];
+   })
+}
+ 
+
+     return (
+         <div>
+           {renderView(state)}  
+         </div>
+     );
+ };
+ 
+
+     
+ export default Folder;
