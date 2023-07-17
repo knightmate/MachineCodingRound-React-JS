3adeb773a7ea8d73729c647b63431ae6a8a211e6
Meghraj Deshmukh <meghraj.deshmukh103@gmail.com>
2023-07-17 23:31:40 +0530

    testing aaudio player

--- src/App.js
+++ src/App.js
@@ -405,4 +405,13 @@ const getMilliSecond=(sec:number)=>{
 
      return sec * 1000;
 }
- 
+const getMinfromSecond=(seconds:number)=>{
+    
+    
+        const format = val => `0${Math.floor(val)}`.slice(-2)
+        const hours = seconds / 3600
+        const minutes = (seconds % 3600) / 60
+      
+        return [ minutes, seconds % 60].map(format).join(':')
+      
+}
