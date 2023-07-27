b51df9120d4b9a3adf243aeac02b6e8a44d11bdf
Meghraj Deshmukh <meghraj.deshmukh103@gmail.com>
2023-07-27 13:31:30 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -58,12 +58,7 @@ function App() {
         ))}
       </div>
 
-      <div className="component-details">
-        {renderComponent()}
-      </div>
-    </div>
-  );
-}
+      
 
 
 export default App;
