72d3868e83562a20f05481064ea3e40160a8b2f4
Meghraj Deshmukh <meghraj.deshmukh103@gmail.com>
2024-02-11 20:31:26 +0530

    not working

--- src/Comment/index.jsx
+++ src/Comment/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { act } from 'react-dom/cjs/react-dom-test-utils.production.min';
 
 
 
@@ -15,6 +16,39 @@ const Comment = ({ onSubmit }) => {
     setCommentItem((pre) => [...pre, comment])
   };
 
+  const findCommentById = (comments, id,action) => {
+    // Search for the comment with the given id in the current level of comments
+    const comment = comments.find(comment => comment.id === id);
+    
+    // If the comment is found, return it
+    if (comment){
+        if(action=="edit"){
+         comment.edit=true
+        }
+        if(action=="delete"){
+          //
+        }
+        if(action=="reply"){
+          //
+        }
+      return comment;
+    }
+    
+    // If the comment is not found at the current level, search recursively in replies
+    for (const comment of comments) {
+      if (comment.replies) {
+        const foundInReplies = findCommentById(comment.replies, id);
+        
+        if (foundInReplies) {
+          return foundInReplies;
+        }
+      }
+    }
+    
+    // If the comment is not found in this level or its replies, return undefined
+    return undefined;
+  };
+  
   return (
     <div>
       <input type="text" value={commentValue} onChange={(event) => setCommentValue(event.target.value)} />
@@ -23,9 +57,10 @@ const Comment = ({ onSubmit }) => {
          
         switch (action) {
           case "edit": {
-
             console.log("EDITED VAL",id,action);
-            
+          const updatedItem=  findCommentById(commentItem,id)
+           
+           setCommentItem((pre)=>[...pre]);
             break; 
           }
           case "delete": {
@@ -54,8 +89,8 @@ const CommentActions = ({
   onChange
 }) => {
 
-  const handleAction = (action) => {
-    onAction(action);
+  const handleAction = (action,id) => {
+    onAction(action,id);
   };
 
 
@@ -65,8 +100,8 @@ const CommentActions = ({
       <div style={{ display: 'flex', flexDirection: 'column', width: 300, border: '2px solid', borderRadius: '5px', padding: '5px' }}>
         <span style={{}}>{value}</span>
         <div style={{ gap: '5px', display: 'flex' }}>
-          <button onClick={() => handleAction('reply',"Emgha")}>Reply</button>
-          <button onClick={() => handleAction('edit',"Emgha")}>Edit</button>
+          <button onClick={() => handleAction('reply',id)}>Reply</button>
+          <button onClick={() => handleAction('edit',id)}>Edit</button>
           <button onClick={() => handleAction('delete',id)}>Delete</button>
           {showSaveButton && <button onClick={onSave}>Save</button>}
           {showCancelButton && <button onClick={onCancel}>Cancel</button>}
