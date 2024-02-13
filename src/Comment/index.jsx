447486dbee2baefa19f0dd791ac56fcba628b6bd
Meghraj Deshmukh <meghraj.deshmukh103@gmail.com>
2024-02-14 01:19:50 +0530

    comment tree -add and edit done

--- src/Comment/index.jsx
+++ src/Comment/index.jsx
@@ -8,23 +8,71 @@ const Comment = ({ onSubmit }) => {
   const [comments, setComments] = useState([]);
   const [commentValue, setCommentValue] = useState();
   const [commentItem, setCommentItem] = useState([]);
-
-  const onAction=(action="Edit"|"Add"|"Delete")=>{
-
-
-     
-  }
   const [commentTree,setCommentTree]=useState({
     id:1,
     item:[{id:3,item:[],value:"Good Product"},{id:4,item:[],value:"Not Product"},{id:5,item:[],value:"Great Product"}]
   });
 
+  const getEdited=(nodes,id,value)=>{
 
-  const handleSubmit = (event) => {
-    const val = event.target.value
-    const comment = { id: new Date().toLocaleString(), comment: commentValue, replay: false, edit: false,replies:[] };
-    setComments((preComments) => [...preComments, comment])
-    setCommentItem((pre) => [...pre, comment])
+      if(nodes.id==id){
+        const tempNode={...nodes,value:value}
+         return tempNode
+     };
+
+     const updatedNodes={...nodes};
+     let items=[]
+      
+     for(let i=0;i<nodes.item.length;i++){
+     items.push(getEdited(nodes.item[i],id,value));
+     };
+
+     return {...updatedNodes,item:items};
+        
+  }
+  const addNode=(nodes,id,node)=>{
+
+    if(nodes.id==id){
+      const tempNode={...nodes,item:[node,...nodes.item]}
+      return tempNode;
+    };
+
+   const updatedNodes={...nodes};
+   let items=[]
+    
+   for(let i=0;i<nodes.item.length;i++){
+   items.push(addNode(nodes.item[i],id,node));
+   };
+
+   return {...updatedNodes,item:items};
+      
+}
+  const onAction=(action,id,value)=>{
+ 
+       
+      switch(action){
+
+        case "Edit":{
+         const updatedTree= getEdited(commentTree,id,value)
+          setCommentTree(updatedTree);
+          return
+        }
+
+        case "Add":{
+         const udpatedNodes= addNode(commentTree,id,{id:Math.random(),value:value,item:[]})
+          setCommentTree(udpatedNodes)
+          return;
+        }
+      }
+     
+  }
+   
+
+
+  const handleSubmit = () => {
+     
+      onAction("Add",commentTree.id,commentValue)
+     
   };
 
   const findCommentById = (comments, id,action) => {
@@ -66,24 +114,7 @@ const Comment = ({ onSubmit }) => {
       <button onClick={handleSubmit}>Comment</button>
      {
       commentTree.item.map((comment)=>{
-        return   <CommentActions  commentItem={comment} onAction={(action, id) => {
-         
-          switch (action) {
-            case "edit": {
-              console.log("EDITED VAL",id,action);
-            const updatedItem=  findCommentById(commentItem,id)
-             
-             setCommentItem((pre)=>[...pre]);
-              break; 
-            }
-            case "delete": {
-  
-            }
-            case "reply": {
-  
-            }
-          }
-        }} />
+        return   <CommentActions parentId={comment.id} commentItem={comment} onAction={onAction}/>
       })
      }
     </div>
@@ -95,49 +126,110 @@ export default Comment;
 
 const CommentActions = ({
    commentItem,
-   onAction
+   onAction,
+   parentId
 }) => {
 
-  const handleAction = (action,id) => {
-    //onAction(action,id);
+  const [isEdit,setIsEdit]=useState(false);
+  const [isReplay,setIsRepaly]=useState(false);
+   const {value,id,item}=commentItem
+  const [_value,setValue]=useState(value)
+  const [replayVal,setReaplyVal]=useState("");
+
+  const handleAction = (id,action,value) => {
+     
+    onAction(id,action,value);
   };
 
    
-  const renderCommentBox = (commentItem) => {
-    const {id,value}=commentItem
-     return (
+  const renderCommentBox = (value, id) => {
+      
+    return (
       <div style={{ display: 'flex', flexDirection: 'column', width: 300, border: '2px solid', borderRadius: '5px', padding: '5px' }}>
-        <span style={{}}>{value}</span>
+       {
+        isEdit ?(<input value={_value} onChange={(event)=>{
+          const val=event.target.value;
+            setValue(val)
+        }}/>):( <span style={{}}>{value}</span>)
+       }
         <div style={{ gap: '5px', display: 'flex' }}>
-          <button onClick={() => handleAction('reply',id)}>Reply</button>
-          <button onClick={() => handleAction('edit',id)}>Edit</button>
-          <button onClick={() => handleAction('delete',id)}>Delete</button>
-            <button onClick={()=>{
-
-            }}>Save</button>
-            <button onClick={()=>{
-
-            }}>Cancel</button>
+          {!isEdit ? (
+            <>
+              <button onClick={() =>setIsRepaly(true)}>Reply</button>
+              <button onClick={() =>{
+                setIsEdit(true)
+                setValue(value)
+              }}>Edit</button>
+              <button onClick={() => handleAction('Delete', id)}>Delete</button>
+            </>
+          ) : (
+            <>
+              <button onClick={()=>{
+              handleAction('Edit',id,_value)
+              setIsEdit(false)
+              }}>Save</button>
+              <button onClick={()=>{
+                setIsEdit(false)
+              }}>Cancel</button>
+            </>
+          )}
         </div>
       </div>
-    )
+    );
+  };
 
-  }
+  const renderReplaybox = (value) => {
+     
+    return (
+      <>
+        <input 
+          placeholder='Reply' 
+          style={{
+            border:'1px solid black',
+            borderColor: 'black',
+            borderWidth: 1,
+            borderRadius: 5, // Adjust border radius as needed
+            padding: '5px 10px', // Adjust padding as needed
+            margin:10 // Adjust margin as needed
+          }} 
+          value={replayVal} 
+          onChange={(event) => {
+            const val = event.target.value;
+            setReaplyVal(val);
+          }}
+        />
+        <button onClick={() => {
+          handleAction('Add', parentId, replayVal);
+          setIsRepaly(false);
+        }}>
+          Save
+        </button>
+        <button onClick={() => {
+          setIsRepaly(false);
+        }}>
+          Cancel
+        </button>
+      </>
+    );
+  };
+  
+  
   
  
    
   return (
       <>
       {
-      commentItem.item.map(({item,value,id})=>{
-       return renderCommentBox(value,id)
-      })
+       renderCommentBox(value,id) 
       }
-      {/* {
-        commentItem?.replay?.length &&  commentItem?.replay?.map((replay)=>{
-          return  <CommentActions  commentItem={replay} onAction={onAction}   />
+      <div style={{marginLeft:'10px'}}> 
+        {isReplay && renderReplaybox(value)}
+      {
+        item?.map((comment)=>{
+          return  <CommentActions  parentId={comment.id}  commentItem={comment} onAction={onAction}/>
         })
-      } */}
+      }
+      </div>
       
       </>
 
